6e46ee5c414e6025040b29a52f45b82dd623ad35
Octavian Pintiliciuc <pintiliciucoctavian@gmail.com>
2020-01-16 15:45:17 +0200

    Initial API commit

---
+++ SmartHomePI.NET.API/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartHomePI.NET.API.Data;
+using SmartHomePI.NET.API.Models;
+
+namespace SmartHomePI.NET.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ValuesController(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await this._dataContext.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            return Ok(await this._dataContext.Values.FirstOrDefaultAsync(value=>value.Id == id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] string value)
+        {
+             this._dataContext.Values.Add(new Value{
+                Id = id,
+                Name = value
+            });
+            
+            await this._dataContext.SaveChangesAsync();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+             this._dataContext.Values.Remove(this._dataContext.Values.Find(id));
+             await this._dataContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ SmartHomePI.NET.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SmartHomePI.NET.API.Models;
+
+namespace SmartHomePI.NET.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}

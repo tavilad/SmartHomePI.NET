16cc07e51674cf7c4a6601c5c12c973e964de50a
Octavian Pintiliciuc <pintiliciucoctavian@gmail.com>
2020-01-25 16:04:21 +0200

    Testing temperature api

--- SmartHomePI.NET.API/Controllers/AuthController.cs
+++ SmartHomePI.NET.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
--- SmartHomePI.NET.API/Controllers/TemperatureController.cs
+++ SmartHomePI.NET.API/Controllers/TemperatureController.cs
@@ -1,5 +1,6 @@
 using Iot.Device.DHTxx;
 using Iot.Units;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SmartHomePI.NET.API.Controllers
@@ -9,7 +10,8 @@ namespace SmartHomePI.NET.API.Controllers
     public class TemperatureController : ControllerBase
     {
         private const int PIN = 4;
-
+        
+        [AllowAnonymous]
         [HttpGet]
         private IActionResult GetTemperature()
         {

c1d185ccb7c58a91e96bcb3030681e4d6e13233e
Octavian Pintiliciuc <pintiliciucoctavian@gmail.com>
2020-01-25 12:56:54 +0200

    Added new components

--- SmartHomePI-NET-Frontend/src/app/app.module.ts
+++ SmartHomePI-NET-Frontend/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,20 +14,25 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { CameraComponent } from './camera/camera.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      TemperatureComponent,
+      CameraComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
--- SmartHomePI.NET.API/DTOs/UserDTO.cs
+++ SmartHomePI.NET.API/DTOs/UserDTO.cs
@@ -8,7 +8,8 @@ namespace SmartHomePI.NET.API.DTOs
         public string Username { get; set; }
 
         [Required]
-        [StringLength(15,MinimumLength = 4,ErrorMessage = "Password must have between 4 and 15 characters")]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Password must have between 4 and 15 characters")]
         public string Password { get; set; }
+        
     }
 }

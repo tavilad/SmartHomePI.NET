0f6b4bf70cd61a866281e58746dae7a653ec1cee
Octavian Pintiliciuc <pintiliciucoctavian@gmail.com>
2020-04-17 20:47:44 +0300

    Generic CRUD service

--- SmartHomePI-NET-Frontend/src/app/_services/room.service.spec.ts
+++ SmartHomePI-NET-Frontend/src/app/_services/room.service.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { RoomService } from './room.service';
+import { RoomService } from './CRUD.service';
 
 describe('Service: Room', () => {
   beforeEach(() => {

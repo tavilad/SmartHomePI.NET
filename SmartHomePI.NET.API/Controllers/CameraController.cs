4a616e0fd94f6d2f18410a098d9bff4e36351e79
Octavian Pintiliciuc <pintiliciucoctavian@gmail.com>
2020-06-20 03:38:31 +0300

    Camera stream?????

--- SmartHomePI.NET.API/Controllers/CameraController.cs
+++ SmartHomePI.NET.API/Controllers/CameraController.cs
@@ -2,10 +2,17 @@ using System;
 using System.Drawing;
 using System.IO;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MMALSharp;
+using MMALSharp.Common;
+using MMALSharp.Common.Utility;
+using MMALSharp.Components;
+using MMALSharp.Handlers;
+using MMALSharp.Ports;
+using MMALSharp.Ports.Outputs;
 using SmartHomePI.NET.API.Helpers;
-using Unosquare.RaspberryIO;
-using Unosquare.RaspberryIO.Camera;
 
 namespace SmartHomePI.NET.API.Controllers
 {
@@ -14,9 +21,13 @@ namespace SmartHomePI.NET.API.Controllers
     public class CameraController : ControllerBase
     {
         private Bitmap frame = null;
+        private MMALCamera cam;
         public CameraController()
         {
-            this.TestCaptureVideo();
+            cam = MMALCamera.Instance;
+            MMALCameraConfig.Resolution = new Resolution(800, 600);
+            cam.ConfigureCameraSettings();
+            this.ChangeVideoEncodingType();
         }
         [HttpGet]
         public IActionResult Stream(int channel)
@@ -46,51 +57,89 @@ namespace SmartHomePI.NET.API.Controllers
                 }
             }
         }
-        private void TestCaptureVideo()
+        protected virtual void OnEmguEventCallback(object sender, EmguEventArgs args)
         {
-            // Setup our working variables
-            var videoByteCount = 0;
-            var videoEventCount = 0;
-            var startTime = DateTime.UtcNow;
+            Console.WriteLine("I'm in OnEmguEventCallback.");
 
-            // Configure video settings
-            var videoSettings = new CameraVideoSettings()
+            using (var ms = new MemoryStream(args.ImageData))
             {
-                CaptureTimeoutMilliseconds = 0,
-                CaptureDisplayPreview = false,
-                ImageFlipVertically = true,
-                CaptureExposure = CameraExposureMode.Night,
-                CaptureWidth = 1920,
-                CaptureHeight = 1080
-            };
-
-            try
-            {
-                // Start the video recording
-                Pi.Camera.OpenVideoStream(videoSettings,
-                    onDataCallback: (data) => { 
-                        using (var ms = new MemoryStream(data))
-                        {
-                            this.frame = new Bitmap(ms);
-                        } },
-                    onExitCallback: null);
-
-                // Wait for user interaction
-                startTime = DateTime.UtcNow;
+                this.frame = new Bitmap(ms);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"{ex.GetType()}: {ex.Message}");
-            }
-            finally
-            {
-                // Always close the video stream to ensure raspivid quits
-                Pi.Camera.CloseVideoStream();
+        }
 
-                // Output the stats
-                var megaBytesReceived = (videoByteCount / (1024f * 1024f)).ToString("0.000");
-                var recordedSeconds = DateTime.UtcNow.Subtract(startTime).TotalSeconds.ToString("0.000");
-            }            
+        public async Task ChangeVideoEncodingType()
+        {
+            // By default, video resolution is set to 1920x1080 which will probably be too large for your project. Set as appropriate using MMALCameraConfig.VideoResolution
+            // The default framerate is set to 30fps. You can see what "modes" the different cameras support by looking:
+            // https://github.com/techyian/MMALSharp/wiki/OmniVision-OV5647-Camera-Module
+            // https://github.com/techyian/MMALSharp/wiki/Sony-IMX219-Camera-Module            
+            using (var vidCaptureHandler = new EmguInMemoryCaptureHandler())
+            using (var splitter = new MMALSplitterComponent())
+            using (var renderer = new MMALNullSinkComponent())
+            {
+                cam.ConfigureCameraSettings();
+                
+                // Register to the event.
+                vidCaptureHandler.MyEmguEvent += this.OnEmguEventCallback;
+
+                // We are instructing the splitter to do a format conversion to BGR24.
+                var splitterPortConfig = new MMALPortConfig(MMALEncoding.BGR24, MMALEncoding.BGR24, 0, 0, null);
+
+                // By default in MMALSharp, the Video port outputs using proprietary communication (Opaque) with a YUV420 pixel format.
+                // Changes to this are done via MMALCameraConfig.VideoEncoding and MMALCameraConfig.VideoSubformat.                
+                splitter.ConfigureInputPort(new MMALPortConfig(MMALEncoding.OPAQUE, MMALEncoding.I420), cam.Camera.VideoPort, null);
+
+                // We then use the splitter config object we constructed earlier. We then tell this output port to use our capture handler to record data.
+                splitter.ConfigureOutputPort<SplitterVideoPort>(0, splitterPortConfig, vidCaptureHandler);
+
+                cam.Camera.PreviewPort.ConnectTo(renderer);
+                cam.Camera.VideoPort.ConnectTo(splitter);
+
+                // Camera warm up time
+                await Task.Delay(2000).ConfigureAwait(false);
+
+                // Record for 10 seconds. Increase as required.
+                var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1000));
+
+                await cam.ProcessAsync(cam.Camera.VideoPort, cts.Token);
+            }
+        }
+
+    public class EmguEventArgs : EventArgs
+    {
+        public byte[] ImageData { get; set; }
+    }
+
+    public class EmguInMemoryCaptureHandler : InMemoryCaptureHandler, IVideoCaptureHandler
+    {
+        public event EventHandler<EmguEventArgs> MyEmguEvent;
+
+        public override void Process(ImageContext context)
+        {
+            // The InMemoryCaptureHandler parent class has a property called "WorkingData". 
+            // It is your responsibility to look after the clearing of this property.
+
+            // The "eos" parameter indicates whether the MMAL buffer has an EOS parameter, if so, the data that's currently
+            // stored in the "WorkingData" property plus the data found in the "data" parameter indicates you have a full image frame.
+
+            // I suspect in here, you will want to have a separate thread which is responsible for sending data to EmguCV for processing?
+            Console.WriteLine("I'm in here");
+
+            base.Process(context);
+
+            if (context.Eos)
+            {
+                this.MyEmguEvent(this, new EmguEventArgs { ImageData = this.WorkingData.ToArray() });
+
+                this.WorkingData.Clear();
+                Console.WriteLine("I have a full frame. Clearing working data.");
+            }
+        }
+
+        public void Split()
+        {
+            throw new NotImplementedException();
         }
     }
+}
 }
